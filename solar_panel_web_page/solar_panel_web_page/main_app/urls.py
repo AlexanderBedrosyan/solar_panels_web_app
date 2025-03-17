3f36f01bc21f22be64784442d6ded25689bf654b
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-03-17 17:04:08 +0200

    Add new page about us, change the styles and update the home-page css

--- solar_panel_web_page/solar_panel_web_page/main_app/urls.py
+++ solar_panel_web_page/solar_panel_web_page/main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home-page')
+    path('', views.HomePage.as_view(), name='home-page'),
+    path('about-us/', views.AboutUs.as_view(), name='about-us')
 ]
