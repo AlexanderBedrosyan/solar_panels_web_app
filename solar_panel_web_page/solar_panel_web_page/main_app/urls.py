d86e70dbee995bdfc8b81ef98fa3c76e6bd22929
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-03-18 10:12:11 +0200

    Update urls

--- solar_panel_web_page/solar_panel_web_page/main_app/urls.py
+++ solar_panel_web_page/solar_panel_web_page/main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home-page'),
-    path('about-us/', views.AboutUs.as_view(), name='about-us')
+    path('about-us/', views.AboutUs.as_view(), name='about-us'),
+    path('services/', views.AboutUs.as_view(), name='services'),
 ]
