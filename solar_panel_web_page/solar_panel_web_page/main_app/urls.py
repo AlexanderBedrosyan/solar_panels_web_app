d8b8ba16fc1c5b246cfe29171614649216270dbd
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-04-04 13:11:50 +0300

    Single project url

--- solar_panel_web_page/solar_panel_web_page/main_app/urls.py
+++ solar_panel_web_page/solar_panel_web_page/main_app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('privacy-policy/', views.PrivacyPolicy.as_view(), name='privacy-policy'),
     path('terms-of-use/', views.TermsOfUse.as_view(), name='terms-of-use'),
     path('cookie-policy/', views.CookiePolicy.as_view(), name='cookie-policy'),
+    path('project-detail/<int:id>', views.ProjectDetailView.as_view(), name='project-detail'),
 ]
